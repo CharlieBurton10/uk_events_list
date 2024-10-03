d8935485e261df219211230427834382e7b9a1d4
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-10-03 12:59:11 +0000

    Add interested button and count

--- static/js/script.js
+++ static/js/script.js
@@ -18,3 +18,23 @@ $(document).ready(function () {
 //     let sidenavs = document.querySelectorAll(".sidenav");
 //     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
 // });
+
+let likes = 0;
+$(document).ready(function () {
+  // ajax to get current likes
+  // let likes from server are 10
+  // assign the current likes to variable
+  likes = 1;
+  setLikes(likes);
+});
+
+$("body").on("click", ".likeBtn", function () {
+  // ajax to post a current likes
+  // in success add increment to likes
+  likes++;
+  setLikes(likes);
+});
+
+function setLikes(count) {
+  $(".totalLikes").text(count);
+}
