73d2a590554a27a6982bebc990569a3fbdcf1722
Charlie Burton <CharlotteMEBurton@hotmail.com>
2024-10-18 08:01:18 +0000

    Sort 404 validate

--- app.py
+++ app.py
@@ -205,8 +205,8 @@ def delete_category(category_id):
 
 # 404 errors
 @app.errorhandler(404)
-def not_found(error):
-    return render_template("404.html", error=error), 404
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
